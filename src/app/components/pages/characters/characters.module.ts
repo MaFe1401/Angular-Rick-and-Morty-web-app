aa3517b464a5fe9453d86c229b3ec23fa097b716
MaFe1401 <marcferre42@gmail.com>
2021-03-13 14:27:45 +0100

    done

---
+++ src/app/components/pages/characters/characters.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharacterDetailsComponent } from './character-details/character-details.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { RouterModule } from "@angular/router"
+import { CharacterComponent } from './character.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
+
+@NgModule({
+  declarations: [
+    CharacterDetailsComponent,
+    CharacterListComponent, 
+    CharacterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    InfiniteScrollModule
+  ],
+  exports: [
+    CharacterDetailsComponent,
+    CharacterListComponent
+  ]
+})
+export class CharactersModule { }
